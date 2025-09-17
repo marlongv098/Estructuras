22e442c9e982c8e65dafa86ce02363b99778320d
Marlon Gomez <marlong@MGPC.local>
2025-09-17 13:19:14 -0500

    ListaEnlazadaDoble

--- 3_Estructuras_NO_Recursivas/3_Generics/1_ListasEnlazadas/2_ListaEnlazadaDoble/src/test/java/ListaDoble/Generica/ListaGTest.java
+++ 3_Estructuras_NO_Recursivas/3_Generics/1_ListasEnlazadas/2_ListaEnlazadaDoble/src/test/java/ListaDoble/Generica/ListaGTest.java
@@ -7,7 +7,7 @@ class ListaGTest {
 
     @Test
     void testInsertAtStart() {
-        System.out.println("Running test: testInsertAtStart");
+
         ListaG<Integer> list = new ListaG<>();
         list.insertAtStart(1);
         assertEquals(1, list.getHeadData());
@@ -16,12 +16,12 @@ class ListaGTest {
         list.insertAtStart(2);
         assertEquals(2, list.getHeadData());
         assertEquals(1, list.getTailData());
-        System.out.println("Test passed.");
+
     }
 
     @Test
     void testInsertAtEnd() {
-        System.out.println("Running test: testInsertAtEnd");
+
         ListaG<String> list = new ListaG<>();
         list.insertAtEnd("a");
         assertEquals("a", list.getHeadData());
@@ -30,24 +30,24 @@ class ListaGTest {
         list.insertAtEnd("b");
         assertEquals("a", list.getHeadData());
         assertEquals("b", list.getTailData());
-        System.out.println("Test passed.");
+
     }
 
     @Test
     void testMixedInsert() {
-        System.out.println("Running test: testMixedInsert");
+
         ListaG<Double> list = new ListaG<>();
         list.insertAtEnd(1.0);
         list.insertAtStart(2.0);
         list.insertAtEnd(3.0);
         assertEquals(2.0, list.getHeadData());
         assertEquals(3.0, list.getTailData());
-        System.out.println("Test passed.");
+
     }
 
     @Test
     void testRemove() {
-        System.out.println("Running test: testRemove");
+
         ListaG<Integer> list = new ListaG<>();
         list.insertAtEnd(1);
         list.insertAtEnd(2);
@@ -73,6 +73,6 @@ class ListaGTest {
         assertEquals(2, list.getHeadData());
         assertEquals(4, list.getTailData());
 
-        System.out.println("Test passed.");
+
     }
 }
